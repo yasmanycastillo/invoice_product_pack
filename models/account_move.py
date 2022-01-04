9b6add88ed6c77da2f8c77e394ef9960258ccd7a
Yasmany Castillo <yasmany003@gmail.com>
2022-01-04 13:13:18 -0400

    REC comment create invoice

--- models/account_move.py
+++ models/account_move.py
@@ -7,24 +7,24 @@ from odoo.exceptions import UserError
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    @api.model_create_multi
-    def create(self, vals_list):
-        for invoice in vals_list:
-            for item in invoice.get('invoice_line_ids'):
-                inv_line = item[2]
-                product_id = inv_line.get('product_id')
-                if inv_line.get('sale_line_ids', False):
-                    so_line_id = inv_line.get('sale_line_ids')[0][1]
-                    so_line = self.env['sale.order.line'].browse([so_line_id])
-                    if so_line.pack_parent_line_id:
-                        pack_line = so_line.pack_parent_line_id
-                        product_pack = pack_line.product_id
-                        pack_qty = len(product_pack.pack_line_ids)
-                        unit_price = pack_line.price_unit / pack_qty
-                        for pl in product_pack.pack_line_ids:
-                            if product_id == pl.product_id.id:
-                                inv_line['price_unit'] = unit_price * pl.quantity
-                    elif so_line.pack_type:
-                        inv_line['price_unit'] = 0
-
-        return super(AccountMove, self).create(vals_list)
+    # @api.model_create_multi
+    # def create(self, vals_list):
+    #     for invoice in vals_list:
+    #         for item in invoice.get('invoice_line_ids'):
+    #             inv_line = item[2]
+    #             product_id = inv_line.get('product_id')
+    #             if inv_line.get('sale_line_ids', False):
+    #                 so_line_id = inv_line.get('sale_line_ids')[0][1]
+    #                 so_line = self.env['sale.order.line'].browse([so_line_id])
+    #                 if so_line.pack_parent_line_id:
+    #                     pack_line = so_line.pack_parent_line_id
+    #                     product_pack = pack_line.product_id
+    #                     pack_qty = len(product_pack.pack_line_ids)
+    #                     unit_price = pack_line.price_unit / pack_qty
+    #                     for pl in product_pack.pack_line_ids:
+    #                         if product_id == pl.product_id.id:
+    #                             inv_line['price_unit'] = unit_price * pl.quantity
+    #                 elif so_line.pack_type:
+    #                     inv_line['price_unit'] = 0
+    #
+    #     return super(AccountMove, self).create(vals_list)
